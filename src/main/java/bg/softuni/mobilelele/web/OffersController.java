72c7d94a93cebaca2146579858872e95fcc02d64
Alex <avd.220900@gmail.com>
2021-10-11 19:13:29 +0300

    Implement OffersController and the related preview views

---
+++ src/main/java/bg/softuni/mobilelele/web/OffersController.java
@@ -0,0 +1,33 @@
+package bg.softuni.mobilelele.web;
+
+import bg.softuni.mobilelele.service.impl.OfferServiceImpl;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/offers")
+public class OffersController {
+
+    private final OfferServiceImpl offerService;
+
+    public OffersController(OfferServiceImpl offerService) {
+        this.offerService = offerService;
+    }
+
+    @GetMapping("/all")
+    public String allOffers(Model model) {
+        model.addAttribute("offers",
+                offerService.getAllOffers());
+        return "offers";
+    }
+
+
+    @GetMapping("/{id}/details")
+    public String showOffer(@PathVariable Long id, Model model){
+        model.addAttribute("offer", this.offerService.findById(id));
+        return "details";
+    }
+}
